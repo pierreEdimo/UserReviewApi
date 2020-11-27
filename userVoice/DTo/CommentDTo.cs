3304b9d326136a00958caf0477af7742f92b89e4
pierre edimo <pierredimo@live.com>
2020-11-26 21:31:41 -0800

    i added a comment feature

---
+++ userVoice/DTo/CommentDTo.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using userVoice.Model;
+using userVoice.OnlyDate;
+
+namespace userVoice.DTo
+{
+    public class CommentDTo
+    {
+        public int Id { get; set; }
+        public int ReviewId { get; set; }
+        public virtual Review Review { get; set; }
+        public string Body { get; set; }
+        public string AuthorId { get; set; }
+        [JsonConverter(typeof(OnlyDateConverter))]
+        public DateTime EntryDate { get; set; } = DateTime.Now;
+       
+    }
+}
---
+++ userVoice/Model/Comment.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using userVoice.OnlyDate;
+
+namespace userVoice.Model
+{
+    public class Comment
+    {
+        public int Id { get; set;  }
+        public int ReviewId { get; set;  }
+        public virtual Review Review { get; set;  }
+        public string Body { get; set;  }
+        public string AuthorId { get; set;  }
+        [JsonConverter(typeof(OnlyDateConverter))]
+        public DateTime EntryDate { get; set; } = DateTime.Now; 
+    
+    }
+}
