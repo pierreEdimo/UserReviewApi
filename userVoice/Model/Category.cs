c0384889501cf01b2f0c946833ecabc1683370df
pierre edimo <pierredimo@live.com>
2020-11-26 21:29:27 -0800

    i added a cateogry feature

---
+++ userVoice/Model/Category.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using userVoice.OnlyDate;
+
+namespace userVoice.Model
+{
+    public class Category
+    {
+        public string Name { get; set;  }
+        [Key]
+        public int Id { get; set;}
+        public virtual List<Item> Items { get; set;  }
+        public string ImageUrl { get; set;  }
+        [JsonConverter(typeof(OnlyDateConverter))]
+        public DateTime EntryDate { get; set; } = DateTime.Now; 
+        
+    }
+}
