1b108faa790a571cba353e10badec79e44e202b9
pierre edimo <pierredimo@live.com>
2021-02-22 20:39:41 +0100

    Add CreateReviewDTO

---
+++ userVoice/DTo/AddReviewDTO.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using userVoice.Helper;
+
+namespace userVoice.DTo
+{
+    public class AddReviewDTO
+    {
+        [ModelBinder(BinderType = typeof(TypeBinder<int>) )]
+        public int ItemId { get; set;  }
+        [ModelBinder(BinderType = typeof(TypeBinder<String>))]
+        public String AuthorId { get; set;  }
+        public double Rate { get; set; }
+        public String Content { get; set; }
+    }
+}
