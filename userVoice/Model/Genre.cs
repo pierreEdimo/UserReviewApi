1179ededf65a44dc692c52ab0cc16f502efae423
pierre edimo <pierredimo@live.com>
2021-02-22 20:43:48 +0100

    Add Genre

---
+++ userVoice/Model/Genre.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Newtonsoft.Json; 
+using System.Threading.Tasks;
+
+namespace userVoice.Model
+{
+    public class Genre
+    {
+       [Key]
+        public int Id { get; set;  }
+        public String Name { get; set;  }
+        public String Picture { get; set;  }
+        [JsonIgnore]
+        public List<Item> Items { get; set;  }
+
+    }
+}
