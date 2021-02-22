9d900d637f09d0f58025f7e68a2abf415a0af742
pierre edimo <pierredimo@live.com>
2021-02-22 20:41:02 +0100

    Add CreateItemDTO

---
+++ userVoice/DTo/CreateItemDTO.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using userVoice.Helper;
+
+namespace userVoice.DTo
+{
+    public class CreateItemDTO
+    {
+        public String Name { get; set;  }
+        public IFormFile Picture { get; set;  }
+        public DateTime OpeningDate { get; set; }
+        public String Description { get; set;  }
+        [ModelBinder(BinderType = typeof(TypeBinder<int>))]
+        public int GenreId { get; set; }
+    }
+}
