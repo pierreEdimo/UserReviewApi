deae81b6f7eacc7e7546d254f0ce49835cfb30c3
pierre edimo <pierredimo@live.com>
2021-02-22 20:41:35 +0100

    Add FilterDTO

---
+++ userVoice/DTo/FilterDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace userVoice.DTo
+{
+    public class FilterDTO
+    {
+        public String Name { get; set;  }
+        public String AuthorId { get; set;  }
+        public int ItemId { get; set;  }
+    }
+}
