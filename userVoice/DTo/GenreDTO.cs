0308db22de818a95c5168a8870b555f3c5d4043d
pierre edimo <pierredimo@live.com>
2021-02-22 20:41:54 +0100

    Add GenreDTO

---
+++ userVoice/DTo/GenreDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using userVoice.Model;
+
+namespace userVoice.DTo
+{
+    public class GenreDTO
+    {
+        public int Id { get; set;  }
+        public String Name { get; set;  }
+        public String Picture { get; set;  }
+        [JsonIgnore]
+        public List<Item> Items { get; set;  }
+    }
+}
