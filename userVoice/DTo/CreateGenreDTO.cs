b199c70e0e7c6de24bb6f2f3553df83f129f976d
pierre edimo <pierredimo@live.com>
2021-02-22 20:40:32 +0100

    Add CreateGenreDTO

---
+++ userVoice/DTo/CreateGenreDTO.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+
+namespace userVoice.DTo
+{
+    public class CreateGenreDTO
+    {
+        public String Name { get; set;  }
+        public IFormFile Picture { get; set;  }
+    }
+}
