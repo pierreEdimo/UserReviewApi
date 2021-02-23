3de914066a173417b5afe59d3a0e95ea41b0c966
pierre edimo <pierredimo@live.com>
2021-02-23 04:24:46 +0100

    add the logininfo

---
+++ userVoice/DTo/LoginInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace userVoice.DTo
+{
+    public class LoginInfo
+    {
+        public string Email { get; set;  }
+        public string passWord { get; set;  }
+    }
+}
