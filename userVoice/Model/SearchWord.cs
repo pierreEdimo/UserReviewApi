3e970ec83a94f85261950d9500de4235f5de4c14
pierre edimo <pierredimo@live.com>
2021-02-23 04:26:32 +0100

    add the searchedword model

---
+++ userVoice/Model/SearchWord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace userVoice.Model
+{
+    public class SearchWord
+    {
+        public int Id { get; set;  }
+        public String UserId { get; set;  }
+        public String Word { get; set;  }
+    }
+}
