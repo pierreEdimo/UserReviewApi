6a531b0fcc28bbae337a7286d36b6a1ec2c33ab9
pierre edimo <pierredimo@live.com>
2021-02-23 04:23:40 +0100

    AddSearchWord dto

---
+++ userVoice/DTo/AddSearchWord.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace userVoice.DTo
+{
+    public class AddSearchWord
+    {
+        public String UserId { get; set; }
+        public String Word { get; set; }
+    }
+}
