9093b0921082979fd485315b8a0e9dc352c6203a
pierre edimo <pierredimo@live.com>
2021-02-23 04:25:35 +0100

    add RegisterInfo DTo

---
+++ userVoice/DTo/RegisterInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace userVoice.DTo
+{
+    public class RegisterInfo
+    {
+        public string UserName { get; set;  }
+        public string passWord { get; set;  }
+        public string Email { get; set;  }
+     
+    }
+}
