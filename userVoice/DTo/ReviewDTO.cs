da782c753cb8fc69b9c5d31cb6b6cf5345002da5
pierre edimo <pierredimo@live.com>
2021-03-04 13:02:24 +0100

    add the userDTO

---
+++ userVoice/DTo/ReviewDTO.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using userVoice.Helper;
+using userVoice.Model;
+
+namespace userVoice.DTo
+{
+    public class ReviewDTO
+    {
+        public String AuthorId { get; set;  }
+        public double Rate { get; set;  }
+        public String Content { get; set;  }
+        [JsonIgnore]
+        public ItemDTO Item { get; set;  }
+        public UserDTO Author { get; set;  }
+        [JsonConverter(typeof(DateConverter) )]
+        public DateTime CreatedAt { get; set; } = DateTime.Now; 
+    }
+}
