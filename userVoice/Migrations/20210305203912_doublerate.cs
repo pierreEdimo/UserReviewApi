59cb8b1e158860b11606cfd010912358477a6aa2
pierre edimo <pierredimo@live.com>
2021-03-05 21:57:58 +0100

    add delete method

---
+++ userVoice/Migrations/20210305203912_doublerate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace userVoice.Migrations
+{
+    public partial class doublerate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Rate",
+                table: "Reviews",
+                type: "REAL",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Rate",
+                table: "Reviews",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "REAL");
+        }
+    }
+}
